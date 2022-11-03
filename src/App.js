cc4d0045dd6a895c76cfdda7eb95afa4bcea22ef
arbaz shah <arbazshah7454@gmail.com>
2022-11-03 14:50:23 +0530

    Create a React app and publish it to github pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur magnam saepe fuga placeat ipsa minima magni est, repellendus ea consequatur iste laudantium blanditiis! Rem cumque repellendus sequi adipisci doloribus facilis?
+      Sunt rerum eius tempora, voluptatum temporibus ipsam labore qui nobis fugit natus beatae totam lorem elit magnam saepa fuga place ipsa? </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus deserunt porro quaerat nihil deleniti voluptatem distinctio impedit, itaque quas_______</p>
+    </div>
+  );
+}
+
+export default App;
